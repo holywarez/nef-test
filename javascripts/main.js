97c9b2d61456c2290f75633c3e9c34ac08624f95
Anatoly Lapshin <holywarez@gmail.com>
2016-11-10 22:09:07 +0400

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,21 @@
 
+var topics = []
+
+topics.push({
+  type: 'matches',
+  combinations: [
+    ['insiencere', "Somebody that doesnt't show his emotions"],
+    ['arrogant', "Person who thinks that he is better than other"],
+    ['stubborn', "No matter - he is right or not, he will act on his own"]
+  ]
+})
+
+topics.push({
+  type: 'illustrations',
+  combinations: [
+    ['Punks', 'https://s-media-cache-ak0.pinimg.com/564x/1d/c5/e6/1dc5e6b56cf185d7571ed7aa878e8065.jpg'],
+    ['Teddy Boys', 'https://static.standard.co.uk/s3fs-public/thumbnails/image/2016/05/24/09/teddyboys2405r.jpg'],
+    ['Goths', 'http://2.bp.blogspot.com/_Wvcp0LJM_HY/TL02iFD2DsI/AAAAAAAAbqg/K80sw68DOHE/s1600/Wave_Gotik_Treffen_Festival__16.jpg']
+  ]
+})
+
