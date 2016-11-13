638de90132993064aea87e4ad9c7d5e1ef855d55
Anatoly Lapshin <holywarez@gmail.com>
2016-11-13 16:27:20 +0400

    more tests!!!!

--- javascripts/topics.js
+++ javascripts/topics.js
@@ -4,6 +4,7 @@ topics.push({
   type: 'matches',
   reversible: true,
   handwriting: true,
+  category: 'Personality',
   combinations: [
     ['insincere', "adjective, She's the kind of person who doesn't say what she really thinks"],
     ['well-balanced', "adjective, She's the kind of person who is emotionally in control, not moody"],
@@ -28,6 +29,7 @@ topics.push({
   type: 'matches',
   reversible: true,
   handwriting: true,
+  category: 'Personality',
   combinations: [
     ['possessive', "adjective, She's not very good at letting other people share her friends"],
     ['reserved', "adjective, She's not very good at showing her feelings or expressing her opinions"],
@@ -43,6 +45,7 @@ topics.push({
     ['funny', "adjective, She has a great sense of humor"],
     ['considerate', 'adjective, Careful not to inconvenience or harm others'],
     ['impatient', 'adjective, Having or showing a tendency to be quickly irritated or provoked'],
+    ['embarrassing', "adjective, Person who has a feeling of self-consciousness, shame, or awkwardness"]
   ]
 })
 
@@ -50,6 +53,7 @@ topics.push({
   type: 'matches',
   reversible: true,
   handwriting: true,
+  category: 'Illness and Treatment',
   combinations: [
     ['nosebleed', "an instance when blood goes from the nose"],
     ['pinch', "grip (something, typically a person's flesh) tightly and sharply between finger and thumb - to press something tightly between finger and thumb"],
@@ -64,6 +68,7 @@ topics.push({
   type: 'matches',
   reversible: true,
   handwriting: false,
+  category: 'Illness and Treatment',
   combinations: [
     ['He has <i>flu</i>', "He's got a temperature and his body aches"],
     ["He's <i>caught a cold</i>", "He's sneezing a lot and he has a cough"],
@@ -83,6 +88,7 @@ topics.push({
   type: 'matches',
   reversible: true,
   handwriting: false,
+  category: 'Illness and Treatment',
   combinations: [
     ["She is unconscious", "She's breathing but her eyes are closed and she can't hear or feel anything"],
     ["She has a sore throat", "It hurts when she talks or shallows food"],
@@ -96,18 +102,8 @@ topics.push({
 topics.push({
   type: 'illustrations',
   reversible: true,
-  handwriting: true,
-  combinations: [
-    ['punks', 'https://s-media-cache-ak0.pinimg.com/564x/1d/c5/e6/1dc5e6b56cf185d7571ed7aa878e8065.jpg'],
-    ['teddy boys', 'https://static.standard.co.uk/s3fs-public/thumbnails/image/2016/05/24/09/teddyboys2405r.jpg'],
-    ['goths', 'http://2.bp.blogspot.com/_Wvcp0LJM_HY/TL02iFD2DsI/AAAAAAAAbqg/K80sw68DOHE/s1600/Wave_Gotik_Treffen_Festival__16.jpg']
-  ]
-})
-
-topics.push({
-  type: 'illustrations',
-  reversible: true,
   handwriting: false,
+  category: 'Illness and Treatment',
   combinations: [
     ['a temperature', 'http://media.gettyimages.com/photos/man-sick-in-bed-taking-his-temperature-picture-id85704286?k=6&m=85704286&s=170667a&w=0&h=RD3ACheFxGeDE2u0CQS7ROi0ELSJZuZ0bJel4c-ushg='],
     ['a cough', 'http://howrid.com/wb-content/uploads/2015/09/how-to-treat-a-cough.jpg'],
@@ -127,6 +123,7 @@ topics.push({
   type: 'matches',
   reversible: true,
   handwriting: true,
+  category: 'Speed Dating',
   combinations: [
     ['prospective partner', "Someone who might become your partner in the future"],
     ['courtship', "The period of time when two people have a romantic relationship before they get married"],
@@ -141,13 +138,30 @@ topics.push({
   type: 'matches',
   reversible: true,
   handwriting: true,
+  category: 'Speed Dating',
   combinations: [
     ['badge', "A small piece of metal, plastic, or cloth with words or a design on it"],
     ['grimace', "An expression on your face that shows you are in pain"],
-    ['giggling', "To laugh in a silly way because you are amused or nervous"],
+    ['giggle', "To laugh in a silly way because you are amused or nervous"],
     ['rise eyebrows', "Move the line of hair above your eye upwards"],
     ['chat up', "Talk to somebody in a friendly way because you are attracted to them"],
-    ['chat', "Talk in a friendly, informal way"]
+    ['chat', "Talk in a friendly, informal way"],
+  ]
+})
+topics.push({
+  type: 'matches',
+  reversible: true,
+  handwriting: true,
+  category: 'Speed Dating',
+  combinations: [
+    ['nickname', "an informal name used by your family or friends"],
+    ['trial', "the noun from the verb <i>try</i>"],
+    ['wary', "careful because you are uncertain or afraid"],
+    ['approach', "go near to somebody"],
+    ['register', "put your name on an official list"],
+    ['come up with', "think of, invent"],
+    ['stunning', "very attractive, beatiful"],
+    ['membership card', "a card which shows you belong to a club or organization"]
   ]
 })
 
@@ -155,10 +169,251 @@ topics.push({
   type: 'matches',
   reversible: true,
   handwriting: false,
+  category: 'Personality',
   combinations: [
     ['annoying, difficult', "I hope Jack doesn't come tonight. He's such <i>a pain in the neck</i>."],
     ['shows no sympathy or fear', "She's <i>as hard as nails</i>. She doesn't care who she hurts."],
     ['distant, unfriendly', "My boss is rather <i>a cold fish</i>. I don't even know if he likes me or not."],
     ['very kind, generous', "She <i>has a heart of gold</i>. She'll always help anyone with anything."],
+  ]
+})
+
+topics.push({
+  type: 'matches',
+  reversible: true,
+  handwriting: false,
+  category: 'Illness and Treatment',
+  combinations: [
+    ['medicine', "Go to the doctor's (GP). He may tell you to take some ______ e.g. <i>antibiotics</i> or <i>painkillers</i>."],
+    ['rest', "Go to the doctor's (GP). He may tell you to stay home and ______"],
+    ['specialist', "Go to the doctor's (GP). He may tell you to go to hospital to see a ______"],
+    ['x-ray', "Go to hospital / to A&E (Accident and Emergency). You may have to have an _____ or <i>scan</i> to see if anything is broken"],
+    ['stiches', "Go to hospital / to A&E (Accident and Emergency). You may have to have _____ if you have very deep cut"],
+    ['injection', "Go to hospital / to A&E (Accident and Emergency). You may have to have an _____, when a <i>drug</i> is put into your body through a <i>needle</i>"],
+    ['bandage', "Go to hospital / to A&E (Accident and Emergency). You may have to have a _____ put on to keep the <i>wound</i> clean"],
+    ['operation', "Go to hospital / to A&E (Accident and Emergency). You may have to have an _____, when part of your body is cut open to remove or repair a damaged part"],
+  ]
+})
+
+topics.push({
+  type: 'matches',
+  reversible: true,
+  handwriting: false,
+  category: 'Illness and Treatment',
+  combinations: [
+    ['cut down', "When we try to do less of something, we try to _________"],
+    ['chronic', "An illness that you have for a very long time is called a ______ illness"],
+    ['beneficial', "Something which is good for us is  ______"],
+    ['strengthen', "The verb to make something stronger is ______"],
+    ['cells', "Our body is made up of millions of _____"],
+    ['damage', "When we treat our body badly we ______ it"],
+    ['disease', "Another word for illness is ______"],
+    ['harmful', "Something which is bad for us is ______"],
+    ['muscles', "Doing exercise helps to make our ______ bigger and stronger"],
+  ]
+})
+
+
+topics.push({
+  type: 'matches',
+  reversible: true,
+  handwriting: false,
+  category: 'Mystical',
+  combinations: [
+    ['birth chart', "A map which shows the position of the planets when a person was born"],
+    ['unalterable', "Which can't be changed"],
+    ['destiny', "What will happen to somebody in the future"],
+    ['genuine', "Real, authentic"],
+    ['scared the life out of me', "Terrified me"],
+    ['curse', "A word  or phrase which has magic power to make something bad happen"],
+  ]
+})
+
+
+
+topics.push({
+  type: 'matches',
+  reversible: true,
+  handwriting: false,
+  category: 'Clothes and Fashion',
+  combinations: [
+    ['key', "very important"],
+    ['dull', "not interesting or exciting"],
+    ['neat', "arranged or done carefully; tidy"],
+    ['braces', "something you wear to hold your trousers up"],
+    ['appalled', "horrified"],
+    ['spiky', "with sharp points"],
+    ['fitted', "made to follow the shape of the body"],
+    ['masterminded', "planned and directed"],
+    ['cuffs', "the end parts of sleeves"],
+    ['tribe', "a social division in a traditional society consisting of families or communities linked by social, economic, religious, or blood ties, with a common culture and dialect"],
+  ]
+})
+
+topics.push({
+  type: 'matches',
+  reversible: true,
+  handwriting: false,
+  category: 'Clothes and Fashion',
+  combinations: [
+    ['dysfunctional', "adjective, unable to deal adequately with normal social relations"],
+    ['innovative', "adjective, (of a product, idea, etc.) featuring new methods; advanced and original"],
+    ['outrageous', "adjective, shockingly bad or excessive; very bold and unusual and rather shocking"],
+    ['macabre', "adjective, disturbing because concerned with or causing a fear of death"],
+    ['conspicuous', "adjective, attracting notice or attention"],
+  ]
+})
+
+topics.push({
+  type: 'illustrations',
+  reversible: true,
+  handwriting: true,
+  category: 'Clothes and Fashion',
+  combinations: [
+    ['fur', "http://www.ecouterre.com/wp-content/uploads/2013/12/fur-coat-2-537x402.jpg"],
+    ['lycra', "http://g01.a.alicdn.com/kf/HTB1mOr4JFXXXXatXpXXq6xXFXXX1/Men-long-johns-men-s-hot-thin-elastic-line-pants-male-fashion-sexy-lycra-long-johns.jpg"],
+    ['linen', "https://halemercantileco.com/wp-content/uploads/2015/07/linen-pillowcase-stripe-basix-floss-rosa.jpg"],
+    ['suede', "http://georgianbaywetcleaners.com/wp-content/uploads/2013/12/Suede-Leather-Cleaning.png"],
+    ['woolen', "https://image.freepik.com/free-photo/woolen--sweater--clothes--wool_3293100.jpg"],
+    ['cotton', "http://www.boldsky.com/img/2014/03/27-cotton-clothes.jpg"],
+    ['denim', "http://www.publicdomainpictures.net/pictures/50000/nahled/denim-cloth.jpg"],
+    ['velvet', "http://ep.yimg.com/ay/yhst-17509616028471/silk-velvet-rose-gold-45-quot-4.gif"],
+    ['nylon', "https://ae01.alicdn.com/kf/HTB11H_IKFXXXXbJXpXXq6xXFXXXh/Abrasion-Resistant-tear-resistant-420D-font-b-nylon-b-font-font-b-fabric-b-font-breathable.jpg"],
+    ['silk', "http://www.thaisilkrestaurantblackheath.com/thaisilkrestaurantblackheath/Home_files/silk-fabric-texture-17.jpg"],
+    ['leather', "http://uncrate.com/p/2016/02/leather-head-process-1.jpg"],
+  ]
+})
+
+
+topics.push({
+  type: 'illustrations',
+  reversible: true,
+  handwriting: true,
+  category: 'Clothes and Fashion',
+  combinations: [
+    ['coat', "http://fabfashionfix.com/wp-content/uploads/2013/02/red_trench_coat_new_look.jpg"],
+    ['anorak', "http://www.first-spear.com/images/P/Anorak_DoubleGroup_0210.jpg"],
+    ['hat', "http://s5o.ru/storage/simple/ru/edt/89/89/62/33/ruec53ecfd19c.jpg"],
+    ['cap', "http://i4.mirror.co.uk/incoming/article8078713.ece/ALTERNATES/s615/Peaky-Blinders-3-Thomas-Shelby-Cillian-Murphy.jpg"],
+    ['gloves', "http://pbguy.com/wp-content/uploads/2014/07/pb-thumb1.jpg"],
+    ['scarf', "http://g03.a.alicdn.com/kf/HTB12ftjKXXXXXX9XVXXq6xXFXXXT/2016-New-Thick-Muslim-Hijab-Shemagh-Tactical-Desert-Arabic-font-b-Scarf-b-font-font-b.jpg"],
+
+  ]
+})
+
+
+topics.push({
+  type: 'illustrations',
+  reversible: true,
+  handwriting: true,
+  category: 'Clothes and Fashion',
+  combinations: [
+    ['boots', "http://letstalksurvival.com/wp/wp-content/uploads/2015/08/LOWA-Zephyr-GTX-Mid-02.jpg"],
+    ['trainers', "http://www.telegraph.co.uk/content/dam/men/2016/01/22/trainers-newbalance2-large_trans++Ssi5XN9JgKI12_IxWcAm00ILJJpwdHwkstMv3u0Srgc.jpg"],
+    ['sandals', "https://static.missguided.co.uk/media/catalog/product/cache/1/image/600x870/9df78eab33525d08d6e5fb8d27136e95/f/3/f3602149_amy_15.06.16_hm_131127.jpg"],
+    ['slippers', "http://ezinemark.com/include/ckfinder/userfiles/images/slippers.jpg"],
+    ['high heels', "http://www.demilked.com/magazine/wp-content/uploads/2015/06/tentacle-shoes-polypodis-kermit-tesoro-thumb6401.jpg"],
+    ['clogs', "http://www.shoebuy.com/pm/swedi/swedi261605_134312_jb1.jpg"],
+    ['flip-flops', "https://ae01.alicdn.com/kf/HTB1Ft.FJpXXXXceXpXXq6xXFXXXA/Colorful-Flowers-Womens-Sandals-Summer-Shoes-Slippers-for-Women-Summer-Shoes-Flower-Flip-Flops-Ladies-Slippers.jpg_640x640.jpg"],
+  ]
+})
+
+
+topics.push({
+  type: 'illustrations',
+  reversible: true,
+  handwriting: false,
+  category: 'Clothes and Fashion',
+  combinations: [
+    ['knickers', "http://redsequinshoes.com/wp-content/uploads/2016/06/knickers.jpg"],
+    ['underpants', "http://thegreenists.com/wp-content/uploads/2011/11/underpants1.gif"],
+    ['bra', "http://debenhams.scene7.com/is/image/Debenhams/152010302960?$V7PdpLarge$"],
+    ['vest', "http://houseoffraser.scene7.com/is/image/HOF/I_152052787_00_20110414?size=450,600&wid=450&hei=600&qlt=80&op_sharpen=1&ResMode=sharp2&pscan=4"],
+    ['tights', "http://data.whicdn.com/images/44601832/large.jpg"],
+    ['boxer shorts', "http://ak1.ostkcdn.com/images/products/8458623/Leisureland-Womens-Smooch-Lips-Cotton-Knit-Boxer-Shorts-b25ef7c8-f32c-4db0-bc9c-3271a2f07839_600.jpg"],
+  ]
+})
+
+
+topics.push({
+  type: 'illustrations',
+  reversible: true,
+  handwriting: false,
+  category: 'Clothes and Fashion',
+  combinations: [
+    ['pocket', "https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/01/22/16/jeans.jpg"],
+    ['sleeve', "http://cdn.shopify.com/s/files/1/0109/6372/files/Sleeve_length_1.png?2415"],
+    ['hood', "http://www.johnfieldclothing.co.uk/wp-content/uploads/2014/12/rain-hood-twister-john-field.jpg"],
+    ['button', "https://davidreevesbespoke.files.wordpress.com/2011/04/coat-button-holes.jpg"],
+    ['collar', "http://www.fashiondesignscope.com/wp-content/uploads/2011/06/dress-shirt-collar.jpg"],
+    ['heel', "http://www.mobisnab.ru/images/Spets/spets-b-08.jpg"],
+    ['zip', "https://whiteroselaundries.co.uk/wp-content/uploads/2015/06/Zip.png"],
+    ['cuff', "https://s-media-cache-ak0.pinimg.com/originals/a8/ae/3e/a8ae3ea981873d4aaf129fc9aab6dc7b.jpg"],
+    ['laces', "https://www.devaskation.com/image/cache/data/accessories%20/Vibe%20LAces-850x850.jpg"],
+  ]
+})
+
+
+topics.push({
+  type: 'illustrations',
+  reversible: true,
+  handwriting: false,
+  category: 'Clothes and Fashion',
+  combinations: [
+    ['necklace', "https://img1.etsystatic.com/000/0/5132181/il_fullxfull.264226399.jpg"],
+    ['ring, bracelet', "https://img0.etsystatic.com/123/0/6886203/il_fullxfull.852994924_ff2f.jpg"],
+    ['belt', "http://www.snakeeatertactical.com/wp-content/uploads/2016/02/Snake-Eater-Tactical-EDC-Belt-Black.jpg"],
+    ['scarf', "https://img1.etsystatic.com/102/1/11649404/il_fullxfull.839915085_bafd.jpg"],
+    ['earring', "http://cdn.supadupa.me/shop/3360/images/2170013/1a_large.jpg?1452840610"],
+    ['watch', "http://images.askmen.com/1080x540/style/watch_snob/watch-forums-1108680-TwoByOne.jpg"],
+    ['brooch', "http://www.enfait.nl/wp-content/uploads/2015/08/insectenbroche.jpg"],
+    ['handbag', "http://houseoffraser.scene7.com/is/image/HOF/I_224711277_01_20150910?size=220,293&wid=220&hei=293&qlt=80&ResMode=sharp2&op_sharpen=1"],
+    ['umbrella', "http://static.boredpanda.com/blog/wp-content/uploads/2015/05/squirrel-umbrella-rain-squirrelisimo-max-ellis-5.jpg"],
+  ]
+})
+
+topics.push({
+  type: 'illustrations',
+  reversible: true,
+  handwriting: false,
+  category: 'Clothes and Fashion',
+  combinations: [
+    ['tight', "http://waterfordwhispersnews.com.s3-eu-west-1.amazonaws.com/wp-content/uploads/2015/05/gym1.jpg"],
+    ['loose', "http://www.dhresource.com/0x0s/f2-albu-g3-M00-6D-F9-rBVaHVU3wHmARjPJAAHxQ26lfRY918.jpg/mens-hip-hop-clothing-casual-loose-blank.jpg"],
+    ['sleeveless', "http://g02.a.alicdn.com/kf/HTB1IJpcKFXXXXcQaXXXq6xXFXXXh/Vintage-Retro-Ethnic-Sleeveless-T-Shirt-Summer-2016-Women-Print-T-Shirts-Short-Navy-Blue-tshirt.jpg_640x640.jpg"],
+    ['long-sleeved', "http://picture-cdn.wheretoget.it/i2x18b-i.jpg"],
+    ['hooded', "http://papini.co.uk/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/n/a/navygrey_hoodie.jpg"],
+    ['V-neck', "http://contentinjection.com/wp-content/uploads/2014/02/green-deep-v-neck.jpg"],
+  ]
+})
+
+topics.push({
+  type: 'illustrations',
+  reversible: true,
+  handwriting: false,
+  category: 'Clothes and Fashion',
+  combinations: [
+    ['spotted', "http://www.polyvore.com/cgi/img-thing?.out=jpg&size=l&tid=92720798"],
+    ['plain', "http://www.unbrandedclothes.co.uk/buyonline/qc31-large.gif"],
+    ['striped', "http://www.soviet-power.com/pictures/Vest.jpg"],
+    ['checked', "https://blueinc_co_uk.secure-cdn.visualsoft.co.uk/images/womens-black-checked-t-shirt-p24839-38149_zoom.jpg"],
+    ['patterned', "https://cdnc.lystit.com/photos/8c9a-2014/11/06/hm-black-patterned-t-shirt-product-1-22282723-0-103167598-normal.jpeg"],
+    ['floral', "https://s-media-cache-ak0.pinimg.com/736x/f6/62/be/f662be7ad4cdfc59d2a626829fa87f7d.jpg"],
+  ]
+})
+
+topics.push({
+  type: 'matches',
+  reversible: true,
+  handwriting: false,
+  category: 'Clothes and Fashion',
+  combinations: [
+    ['get undressed', "Can you tell the children to _________ and get into the bath, please?"],
+    ['dress up', "The party's going to be quite formal, so I suppose we'd better ________"],
+    ['hang up', "Can you _______ the shirts I've just ironed in your wardrobe, please?"],
+    ['fit', "I think I must have put on a bit of weight. These trousers don't _____ me any more"],
+    ['suits', "You look great in that new dress! It really suits you"],
+    ['get changed', "I've been working in the garden, so I'll have to ________ before we go out"],
+    ['match', "Ruth bought a red handbag to _______ her red shoes"],
   ]
 })
